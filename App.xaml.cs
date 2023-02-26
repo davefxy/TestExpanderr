f95af1795fe85ab0d83dc0a8a84dd63dc229263c
Davefxy <davefxy@yahoo.com>
2023-02-26 08:17:01 -0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TestExpanderr;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace TestExpanderr;
+
+public partial class MainPage : ContentPage
+{
+    ExpanderViewModel viewModel;
+
+    public MainPage(ExpanderViewModel vm) {
+        InitializeComponent();
+        BindingContext = viewModel = vm;
+    }
+
+    async void Expander_ExpandedChanged(object sender, ExpandedChangedEventArgs e) {
+        var collapsedText = e.IsExpanded ? "expanded" : "collapsed";
+        await Toast.Make($"Expander is {collapsedText}").Show(CancellationToken.None);
+    }
+}
+
